ae085be70bed97fddc418731aebaf41cde8edf78
Rikard Funck <rikard.funck@stratiteq.com>
2023-12-06 09:15:34 +0100

    aoc01 part 1 and 2

--- AOC01/Program.cs
+++ AOC01/Program.cs
@@ -1,2 +1,45 @@
-﻿// AOC01 #1 TBD
-Console.WriteLine("Hello World!");
+﻿// AOC01 #1 + #2
+using System.Text.RegularExpressions;
+using Shared;
+
+var dataAsRows = Helpers.ReadContentAndSplitFromInputFile();
+var sumOfAll = 0;
+var numbersAsWords = new (string, string)[] { ("one", "1"), ("two", "2"), ("three", "3"), ("four", "4"), ("five", "5"), ("six", "6"), ("seven", "7"), ("eight", "8"), ("nine", "9") };
+
+foreach (var row in dataAsRows)
+{
+    var matched = new Dictionary<int, string>();
+
+    foreach (var wordTuple in numbersAsWords)
+    {
+        var numericMatches = Regex.Matches(row, wordTuple.Item2);
+
+        if (numericMatches.Count > 0)
+        {
+            foreach (Match numericMatch in numericMatches)
+            {
+                matched.Add(numericMatch.Index, wordTuple.Item2);
+            }
+        }
+
+        var alphabeticalMatches = Regex.Matches(row, wordTuple.Item1);
+
+        if (alphabeticalMatches.Count > 0)
+        {
+            foreach (Match alphabeticalMatch in alphabeticalMatches)
+            {
+                matched.Add(alphabeticalMatch.Index, wordTuple.Item2);
+            }
+        }
+    }
+
+    var firstString = matched.MinBy(item => item.Key).Value;
+    var lastString = matched.MaxBy(item => item.Key).Value;
+
+    if (int.TryParse($"{firstString}{lastString}", out int result))
+    {
+        sumOfAll += result;
+    }
+}
+
+Console.WriteLine($"Sum: {sumOfAll}");
---
+++ Shared/Helpers.cs
@@ -0,0 +1,12 @@
+﻿namespace Shared;
+
+public class Helpers
+{
+    public static string[] ReadContentAndSplitFromInputFile(string filePath = "Data/input.txt", string separator = "\r\n")
+    {
+        var path = Path.Combine(Directory.GetCurrentDirectory(), filePath);
+        var fileContent = File.ReadAllText(path).Split(separator);
+
+        return fileContent;
+    }
+}
