bcb12b32aad7b45effae0730c7c59ace7a4964e9
Rikard Funck <st.rikard.funck@regionskane.onmicrosoft.com>
2023-12-10 13:16:43 +0100

    aoc02 part 1 and 2

---
+++ AOC02/Program.cs
@@ -0,0 +1,36 @@
+﻿using AOC02.Utilities;
+using Shared;
+using System.Text.RegularExpressions;
+
+// AOC02 #2
+var blueMax = 14;
+var greenMax = 13;
+var redMax = 12;
+var sum = 0;
+var powerOfSum = 0;
+
+var dataAsRows = Helpers.ReadContentAndSplitFromInputFile();
+
+foreach (var row in dataAsRows)
+{
+    var cleaned = Regex.Replace(row, "[^0-9a-zA-z\\s]", string.Empty);
+    var splitted = cleaned.Split(' ').ToList();
+
+    var blue = splitted.GetValuesWithOffsettFromList("blue", -1).Max();
+    var red = splitted.GetValuesWithOffsettFromList("red", -1).Max();
+    var green = splitted.GetValuesWithOffsettFromList("green", -1).Max();
+
+    var id = splitted.GetValuesWithOffsettFromList("Game", 1).First();
+
+    if ((green != -1 && green <= greenMax) && (red != -1 && red <= redMax) && (blue != -1 && blue <= blueMax))
+    {
+        sum += id;
+    }
+
+    powerOfSum += blue * red * green;
+}
+
+Console.WriteLine($"Sum: {sum}");
+
+// AOC02 #2
+Console.WriteLine($"PowerOfSum: {powerOfSum}");
---
+++ AOC02/Utilities/Methods.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC02.Utilities;
+
+public static class Methods
+{
+    public static List<int> GetValuesWithOffsettFromList(this List<string> data, string identifier, int indexOffset)
+    {
+        var occurances = new List<int>();
+
+        while (true)
+        {
+            var index = data.IndexOf(identifier);
+
+            if (index != -1)
+            {
+                var values = int.Parse(data[index + indexOffset]);
+                occurances.Add(values);
+
+                var startIndex = index + indexOffset;
+                var amountToRemove = 2;
+
+                if (indexOffset > 0)
+                {
+                    startIndex = index;
+                    amountToRemove = indexOffset;
+                }
+
+                data.RemoveRange(startIndex, amountToRemove);
+
+                continue;
+            }
+            break;
+        }
+
+        return occurances;
+    }
+}
